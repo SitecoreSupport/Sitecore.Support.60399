49f60956c1ec50fec84280327bbbba82a8f759ab
ido <ido@WS-IDO1UA1>
2017-04-28 17:58:49 +0300

    First commit - patch is ready

---
+++ src/Sitecore.Support.60399/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.60399")]
+[assembly: AssemblyProduct("Sitecore.Support.60399")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Treecrumb in the Experience Editor does not change the sc_site parameter in URL")]
+

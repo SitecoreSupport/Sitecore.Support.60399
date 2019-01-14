cf0577f80013820afda8b5829518949db0a34483
DK\dmsa <dmsa@sitecore.net>
2019-01-14 12:56:10 +0200

    Update the fix for Sitecore 9.0 Update-2

--- src/Sitecore.Support.60399/ExperienceEditor/Speak/Ribbon/Requests/Treecrumb/ContextSite.cs
+++ src/Sitecore.Support.60399/ExperienceEditor/Speak/Ribbon/Requests/Treecrumb/ContextSite.cs
@@ -19,63 +19,30 @@ namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.Treecrumb
     {
       PipelineProcessorResponseValue result = new PipelineProcessorResponseValue();
 
-      if (base.RequestContext != null && !string.IsNullOrWhiteSpace(base.RequestContext.Argument))
-      {
-        var uri = new Uri(base.RequestContext.Argument);
-        if (!string.IsNullOrWhiteSpace(uri.Query))
-        {
-          var parts = uri.Query.Split(new char[] { '/', '&' }, StringSplitOptions.RemoveEmptyEntries);
-          int scItemIndex = -1;
-          for (int i = 0; i < parts.Length; i++)
-          {
-            if (parts[i].StartsWith("sc_itemid=", StringComparison.InvariantCultureIgnoreCase))
-            {
-              scItemIndex = i;
-              break;
-            }
-          }
-          if (scItemIndex >= 0)
-          {
-            var itemParts = parts[scItemIndex].Split(new char[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
-            if (itemParts.Length == 2 && !string.IsNullOrWhiteSpace(itemParts[1]))
-            {
-              string itemId = itemParts[1];
-              itemId = HttpContext.Current.Server.UrlDecode(itemId);
-              if (ID.IsID(itemId))
-              {
-                var db = Sitecore.Context.Database ?? Sitecore.Context.ContentDatabase;
-                if (db != null)
-                {
-                  var item = db.GetItem(itemId);
-                  if (item != null)
-                  {
-                    var site = GetSiteContext(item.Paths.FullPath);
-                    if (site != null)
-                    {
-                      int scSiteIndex = -1;
-                      for (int i = 0; i < parts.Length; i++)
-                      {
-                        if (parts[i].StartsWith("sc_site=", StringComparison.InvariantCultureIgnoreCase))
-                        {
-                          scSiteIndex = i;
-                          break;
-                        }
-                      }
-                      if (scSiteIndex >= 0)
-                      {
-                        string newUrl = base.RequestContext.Argument.Replace(parts[scSiteIndex], "sc_site=" + site.Name);
-                        result.Value = newUrl;
-                      }                      
-                    }
-                  }
-                }
-              }
-            }
-          }          
-        }
-      }
+			if (RequestContext != null && !string.IsNullOrWhiteSpace(RequestContext.Argument))
+			{
+				if (!string.IsNullOrEmpty(RequestContext.Argument))
+				{
+					if (ID.IsID(RequestContext.Argument))
+					{
+						ID itemId = ID.Parse(RequestContext.Argument);
+						var db = Context.Database ?? Context.ContentDatabase;
 
-      return result;
+						if (db != null)
+						{
+							var item = db.GetItem(itemId);
+
+							if (item != null)
+							{
+								var site = GetSiteContext(item.Paths.FullPath);
+								result.Value = site?.Name;
+							}
+						}
+					}
+				}
+			}
+
+			return result;
     }
 
     protected virtual SiteContext GetSiteContext(string fullPath)
